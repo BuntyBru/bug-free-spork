f6985cb0cdae41b3a7f6215c96b38ae6a79c1594
BuntyBru <buntybru@gmail.com>
2021-07-18 14:47:21 +0530

    changes

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,8 +8,13 @@ const Cart = (props) => {
   const cartCtx = useContext(CartContext);
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
-  const cartItemRemoveHandler = () => {};
-  const cartItemAddHandler = () => {};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+  const cartItemAddHandler = (item) => {
+console.log(item)
+    cartCtx.addItem({...item,amount:1})
+  };
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
@@ -20,7 +25,7 @@ const Cart = (props) => {
           amount={item.amount}
           price={item.price}
           onRemove={cartItemRemoveHandler.bind(null,item.id)}
-          onA dd={cartItemAddHandler.bind(null,item)}
+          onAdd={cartItemAddHandler.bind(null,item )}
         />
       ))}
     </ul>
